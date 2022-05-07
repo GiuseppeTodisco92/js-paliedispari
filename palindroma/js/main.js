bad93082ba80f5d64a397faa72b582e4828323e6
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-07 16:04:18 +0100

    add function metodo 2

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -5,7 +5,8 @@ Creare una funzione per capire se la parola inserita è palindroma */
 let word = prompt("inserire una parola");
 //metodo 1 con funzioni javascript
 palindrome(word);
-
+//metodo 2 con funzione_custom
+palindrome_2(word);
 
 // metodo 1 
 function palindrome (string) {
@@ -19,6 +20,29 @@ if ( string === stringReverse){
 } else {
     console.log("la parola", string , "non è palindroma");
 }
+}
+
+// metodo 2
+
+function palindrome_2 (string){
+    let i = string.length - 1; // passo la lunghezza della parola ad i per utilizzarlo come contatore dell'array, -1 per prendere anche la posizione 0 dell'array
+   let char; // variabile che utilizzo per prendere i caratteri al contrario
+   let stringReverse = ''; // dichiaro una parola vuota da utilizzare come contenitore della parola invertita 
+   
+
+   //while per scorrere la parola 
+   while (i >= 0){
+        char = string[i]; //passo l'ultima lettera a char e la ricmopongo in stringReverse 
+        stringReverse += char; // ricompongo la parola al contrario
+        i--; // decremento il contatore per scorrere la parola al contrario 
+   }
+    console.log(stringReverse);
+
+    if ( string === stringReverse){
+        console.log("la parola", string , "è palindroma");
+    } else {
+        console.log("la parola", string , "non è palindroma");
+    }
 }
 
 
